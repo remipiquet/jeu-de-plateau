b809a399863b0b83fdebeeb69931f696552b52cd
remipiquet <piquet.remi@gmail.com>
2018-08-14 17:09:32 +0200

    définition du début de la fonction players et ménage dans le fichier players.js

--- players.js
+++ players.js
@@ -1,4 +1,4 @@
-    // Fichier js des joueurs 
+    // Fichier JS des joueurs 
 
 // /!\IL RESTE TOUT A FAIRE/!\
     this.initPlayers = function () {
@@ -7,24 +7,15 @@
         }
     }
     
-    this.initRocks = function () { // on génère le coté Y du plateau par rapport à la mapSize
-			//console.log('    y = '+ y); //histoire de comprendre ce que ca fait en console
-			
-			//pour chaque colonne de chaque ligne on mets ou non un rocher
-			if(Math.floor((Math.random() * this.statRocks)) == 0){ // on genere un nombre entre 0 et statRocks compris et si on a 0 on met un rocher (1/10 de rochers quoi)
-				this.plateau[x][y] = 4;
-            }else{    // dans ce cas la case est vide
-				this.plateau[x][y] = 3; // TODO: vérifier que l'initialisation a 0 est utile dans bien des langages de prog les tableaux sont par défaut a 0
-                // j'ai mis 4 quand y'a un rocher et 3 quand c'est vide comme ça on réserve 1 et 2 pour les joueurs 
-            }
 
-    }  
     
 
 
-function players (){
-    this.life = 100;
+function players (name, health, force, weapon, position){
+    this.name = name;
+    this.health = health;
+    this.force = force;
     this.weapon = weapon;
-    this.positionX = x;
-    this.positionY = y;
+    this.position = position;
+
 }
