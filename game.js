bcc4efe0136f47b5911d84b8c09607728041b555
remipiquet <piquet.remi@gmail.com>
2018-09-03 16:31:35 +0200

    Ajout des 4 armes
    On peut surement simplifier pour ne pas rappeler 3 fois les emptycells et faire moins de lignes

--- game.js
+++ game.js
@@ -1,14 +1,9 @@
-// initialisation du jeu
+// ****** Initialisation du jeu ******
+
 var myGame = new Board(5, 4);
 
 
-// initialisation du joueur 1
-
-/*player1[X][Y] = Math.floor(Math.random()*(emptyCells));
-console.log(player1);*/
-//myGame.contenuCase(1, 2);
-//myGame.afficheTout();
-//myGame.getEmptyCells();
+// ****** Placement des joueurs sur le plateau ******
 
 var boardEmptyCells = myGame.getEmptyCells(); // On prend les cases vides
 
@@ -22,17 +17,17 @@ myGame.board[player1Json.emptyX][player1Json.emptyY] = "Joueur 1"; // On donne �
 
 var found = false; // condition de la boucle while
 
-while (!found){
+while (!found){ // tant que found n'est pas égal à "true"...
   var player2Position = Math.floor(Math.random()*(boardEmptyCells.length)); // On prend une position au hasard dans les cases vides
   var player2Json = boardEmptyCells[player2Position]; // On stocke le placement du joueur 2 en JSON dans ces cases vides
 
     if (player1Position != player2Position) { // On vérifie que les joueurs soient éloignés d'au moins 1 case
       if (player1Json.emptyX == player2Json.emptyX) {
-        if (player2Json.emptyY != player1Json.emptyY + 1 && player2Json.emptyY != player1Json.emptyY - 1); {
+        if (player2Json.emptyY != player1Json.emptyY + 1 && player2Json.emptyY - 1 != player1Json.emptyY - 1); {
           myGame.board[player2Json.emptyX][player2Json.emptyY] = "Joueur 2";
             found = true;
           }
-      }
+
       if (player1Json.emptyY == player2Json.emptyY) {
         if (player2Json.emptyX != player1Json.emptyX + 1 && player2Json.emptyX != player1Json.emptyX - 1); {
           myGame.board[player2Json.emptyX][player2Json.emptyY] = "Joueur 2";
@@ -40,31 +35,51 @@ while (!found){
         }
       }
     }
+  }
 }
 
+// ****** Placement des armes sur le plateau ******
 
+// Placement de l'arme 1 (Couteau)
 var boardEmptyCells = myGame.getEmptyCells(); // On reprend les cases vides cette fois sans celles occupées par les joueurs
 
+var weapon1Position = Math.floor(Math.random()*(boardEmptyCells.length)); // On prend une position au hasard dans les cases vides
+
+var weapon1Json = boardEmptyCells[weapon1Position]; // On stocke le placement de l'arme 1 en JSON dans ces cases vides
+
+  myGame.board[weapon1Json.emptyX][weapon1Json.emptyY] = "Couteau"; // On donne à la case le résultat 5 (pour signifier l'arme n°1 sur le plateau)
+
+
+// Placement de l'arme 2 (Fourche)
+var boardEmptyCells = myGame.getEmptyCells();
+
+var weapon2Position = Math.floor(Math.random()*(boardEmptyCells.length)); // On prend une position au hasard dans les cases vides
+
+var weapon2Json = boardEmptyCells[weapon2Position]; // On stocke le placement de l'arme 2 en JSON dans ces cases vides
+
+  myGame.board[weapon2Json.emptyX][weapon2Json.emptyY] = "Fourche"; // On donne à la case le résultat 6 (pour signifier l'arme n°2 sur le plateau)
+
+
+// Placement de l'arme 3 (Flingue)
+var boardEmptyCells = myGame.getEmptyCells();
+
+var weapon3Position = Math.floor(Math.random()*(boardEmptyCells.length)); // On prend une position au hasard dans les cases vides
+
+var weapon3Json = boardEmptyCells[weapon3Position]; // On stocke le placement de l'arme 3 en JSON dans ces cases vides
+
+  myGame.board[weapon3Json.emptyX][weapon3Json.emptyY] = "Flingue"; // On donne à la case le résultat 7 (pour signifier l'arme n°3 sur le plateau)
+
+
+// Placement de l'arme 4 (Lance-flame)
+var boardEmptyCells = myGame.getEmptyCells();
+
+var weapon4Position = Math.floor(Math.random()*(boardEmptyCells.length)); // On prend une position au hasard dans les cases vides
+
+var weapon4Json = boardEmptyCells[weapon4Position]; // On stocke le placement de l'arme 2 en JSON dans ces cases vides
+
+  myGame.board[weapon4Json.emptyX][weapon4Json.emptyY] = "Lance-flame"; // On donne à la case le résultat 6 (pour signifier l'arme n°2 sur le plateau)
 
 
 
-  var weapon1Position = Math.floor(Math.random()*(boardEmptyCells.length)); // On prend une position au hasard dans les cases vides
-
-  var weapon1Json = boardEmptyCells[player2Position]; // On stocke le placement de l'arme 1 en JSON dans ces cases vides
-
-
-  myGame.board[weapon1Json.emptyX][weapon1Json.emptyY] = "Flingue"; // On donne à la case le résultat 5 (pour signifier l'arme n°1 sur le plateau)
-
-
-
-
+// Commande pour voir le plateau en textuel
 myGame.afficheTout();
-
-
-/*
-// à refaire avec myPosition comme ça on peut faire player1.myPosition
-whereIsPlayer1 = function() {
-  player1Position = Math.floor(Math.random()*(emptyCells));
-};
-      console.log("Le joueur 1 est en position :"+player1Position);
-*/
